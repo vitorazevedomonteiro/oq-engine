8c6bd40eba3e14c5a15c6ce13fbd274ea7de62d4
Vitor Monteiro <vitormonteiro@iusspavia.it>
2026-02-23 18:28:28 +0100

    Update: create test for LothBaker2013CorrelationModel

--- openquake/hazardlib/tests/correlation/cross_spatial_correlation_test.py
+++ openquake/hazardlib/tests/correlation/cross_spatial_correlation_test.py
@@ -35,7 +35,7 @@ class LothBaker2013CorrelationMatrixTestCase(unittest.TestCase):
     def test(self):
         cormo = LothBaker2013CorrelationModel()
         imts = [SA(period=0.1, damping=5), SA(period=1.0, damping=5)]
-        corma = cormo.get_correlation_model(self.SITECOL, imts)
+        corma = cormo._get_correlation_matrix(self.SITECOL, imts)
         aaae(corma, [[1,    0.31737876, 1,  0.31737876, 0.2,    0.11200214,
                       0.2,    0.11200214],
                      [0.31737876,   1,  0.31737876, 0.1726039,
@@ -79,11 +79,15 @@ class LothBaker2013LowerTriangleCorrelationMatrixTestCase(unittest.TestCase):
                               Site(Point(2, -39.9), 1, 1, 1)])
 
     def test(self):
-        cormo = LothBaker2013CorrelationModel(vs30_clustering=False)
-        lt = cormo.get_lower_triangle_correlation_matrix(self.SITECOL, PGA())
-        aaae(lt, [[1.0,            0.0,            0.0],
-                  [1.97514806e-02, 9.99804920e-01, 0.0],
-                  [1.97514806e-02, 5.42206860e-20, 9.99804920e-01]])
+        cormo = LothBaker2013CorrelationModel()
+        imts = [SA(period=0.5, damping=5), SA(period=5.0, damping=5)]
+        lt = cormo.get_lower_triangle_correlation_matrix(self.SITECOL, imts)
+        aaae(lt, [[0.99498744, 0, 0, 0, 0, 0],
+                  [0.32087354, 0.9418281, 0, 0, 0, 0],
+                  [0.32087354, 0.07432345, 0.93889094, 0, 0, 0],
+                  [0.33166248, 0.03488297, 0.03223073, 0.94220187, 0, 0],
+                  [0.13997712, 0.30269329, 0.01335984, 0.34923708, 0.87557875, 0],
+                  [0.13997712, 0.03720492, 0.30069504, 0.34923708, 0.06047969, 0.87348747]])
 
 
 class LothBaker2013ApplyCorrelationTestCase(unittest.TestCase):
@@ -93,29 +97,64 @@ class LothBaker2013ApplyCorrelationTestCase(unittest.TestCase):
 
     def test(self):
         numpy.random.seed(13)
-        cormo = LothBaker2013CorrelationModel(vs30_clustering=False)
-        intra_residuals_sampled = numpy.random.normal(size=(3, 100000))
+        cormo = LothBaker2013CorrelationModel()
+        
+        # Two IMTs --> cross-IMT correlation
+        imts = [SA(period=0.5, damping=5), SA(period=5.0, damping=5)]
+        num_sites = len(self.SITECOL)
+        num_imts = len(imts)
+        num_realizations = 100000
+        
+        # Sample uncorrelated residuals
+        intra_residuals_sampled = numpy.random.normal(size=(num_sites * num_imts, num_realizations))
+        
+        # Apply correlation
         intra_residuals_correlated = cormo.apply_correlation(
-            self.SITECOL, PGA(), intra_residuals_sampled
+            self.SITECOL, imts, intra_residuals_sampled
         )
-        inferred_corrcoef = numpy.corrcoef(intra_residuals_correlated)
+        
+        # Flatten IMT x site for correlation computation
+        flattened = intra_residuals_correlated.reshape(num_sites * num_imts, num_realizations)
+        inferred_corrcoef = numpy.corrcoef(flattened)
+        
+        # Check mean and std of correlated residuals
         mean = intra_residuals_correlated.mean()
         std = intra_residuals_correlated.std()
         self.assertAlmostEqual(mean, 0, delta=0.002)
         self.assertAlmostEqual(std, 1, delta=0.002)
 
-        actual_corrcoef = cormo._get_correlation_matrix(self.SITECOL, PGA())
+        # Compare to the correlation matrix from the model
+        actual_corrcoef = cormo._get_correlation_matrix(self.SITECOL, imts)
         numpy.testing.assert_almost_equal(inferred_corrcoef, actual_corrcoef,
                                           decimal=2)
 
     def test_filtered_sitecol(self):
         filtered = self.SITECOL.filtered([0, 2])
+        filtered.indices = numpy.array([0, 2])
+        
         numpy.random.seed(13)
-        cormo = LothBaker2013CorrelationModel(vs30_clustering=False)
-        intra_residuals_sampled = numpy.random.normal(size=(2, 5))
+        cormo = LothBaker2013CorrelationModel()
+        imts = [SA(period=0.5, damping=5), SA(period=5.0, damping=5)]
+        
+        num_imts = len(imts)
+        num_sites_filtered = len(filtered)
+        num_realizations = 5
+        
+        # Sample uncorrrelated residuals
+        intra_residuals_sampled = numpy.random.normal(size=(num_sites_filtered * num_imts, num_realizations))
+        # Apply correlation
         intra_residuals_correlated = cormo.apply_correlation(
-            filtered, PGA(), intra_residuals_sampled)
-        aaae(intra_residuals_correlated,
-             [[-0.71239066, 0.75376638, -0.04450308, 0.45181234, 1.34510171],
-              [0.51816327, 1.36481251, 0.86016437, 1.48732124, -1.01860545]],
-             decimal=6)
+            filtered, imts, intra_residuals_sampled)
+        
+        # Assert expected correlated residuals
+        aaae(
+            intra_residuals_correlated,
+            [
+                [[-0.70881976, 0.74998808, -0.04428, 0.4495476, 1.3383593 ],
+                 [ 0.27121991, 1.50954287, 0.7943037, 1.53329927, -0.54988757]],
+                [[-0.96250255, -0.89517391, 0.54331276, -0.03175413, 1.2733547 ],
+                 [0.0620108, 0.1821031, 2.32763212, 0.95248385, 0.16967118]],
+            ],
+            decimal=6
+        )
+             
