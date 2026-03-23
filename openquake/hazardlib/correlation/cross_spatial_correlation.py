7c51fd42bad77331da8a02145841fbea469bb022
Vitor Monteiro <vitormonteiro@iusspavia.it>
2026-03-23 09:08:18 +0100

    Update: MarkhvidaEtAl2018 name

--- openquake/hazardlib/correlation/cross_spatial_correlation.py
+++ openquake/hazardlib/correlation/cross_spatial_correlation.py
@@ -283,7 +283,7 @@ def get_nugget_cov(var_model: Dict, dist: np.ndarray) -> np.ndarray:
     return np.eye(len(dist)) * var_model["Cn"]
 
 
-class MarkhvidaEtAl2018CorrelationModel(BaseSpatialCrossCorrelationModel):
+class MarkhvidaEtAl2018CorrelationMode(BaseSpatialCrossCorrelationModel):
     """
     Implements the spatial cross-correlation model of Markhvida et al. (2018)
     based on principal component analysis and geostatistics.
