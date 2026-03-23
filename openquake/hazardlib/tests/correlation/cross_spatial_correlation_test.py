472a228abee8499fe4329386b41a348f63aa67fc
Vitor Monteiro <vitormonteiro@iusspavia.it>
2026-03-23 12:17:55 +0100

    Update: Add test for WangDu2013 cross-IM spatial correlation model

--- openquake/hazardlib/tests/correlation/cross_spatial_correlation_test.py
+++ openquake/hazardlib/tests/correlation/cross_spatial_correlation_test.py
@@ -17,9 +17,10 @@
 import unittest
 import numpy
 
-from openquake.hazardlib.imt import SA, Sa_avg2, Sa_avg3, PGA, FIV3, CAV
+from openquake.hazardlib.imt import SA, Sa_avg2, Sa_avg3, PGA, FIV3, CAV, PGV
 from openquake.hazardlib.correlation.cross_spatial_correlation import (
     LothBaker2013CorrelationModel,
+    WangDu2013CorrelationModel,
     MarkhvidaEtAl2018CorrelationModel,
     MonteiroEtAlGlobal2026CorrelationModel,
     DuNing2021CorrelationModel,
@@ -75,27 +76,6 @@ class LothBaker2013CorrelationMatrixTestCase(unittest.TestCase):
                          1,         0.38646643, 1,         0.38646643],
                      [0.13927548, 0.0799556,  0.13927548, 0.33,
                       0.38646643, 0.20979378, 0.38646643, 1]])
-
-
-class LothBaker2013LowerTriangleCorrelationMatrixTestCase(unittest.TestCase):
-    SITECOL = SiteCollection([Site(Point(2, -40), 1, 1, 1),
-                              Site(Point(2, -40.1), 1, 1, 1),
-                              Site(Point(2, -39.9), 1, 1, 1)])
-
-    def test(self):
-        cormo = LothBaker2013CorrelationModel()
-        imts = [SA(period=0.5, damping=5), SA(period=5.0, damping=5)]
-        lt = cormo.get_lower_triangle_correlation_matrix(self.SITECOL, imts)
-        aaae(lt, [
-            [0.99498744, 0, 0, 0, 0, 0],
-            [0.32087354, 0.9418281, 0, 0, 0, 0],
-            [0.32087354, 0.07432345, 0.93889094, 0, 0, 0],
-            [0.33166248, 0.03488297, 0.03223073, 0.94220187, 0, 0],
-            [0.13997712, 0.30269329, 0.01335984, 0.34923708, 0.87557875, 0],
-            [0.13997712, 0.03720492, 0.30069504, 0.34923708, 0.06047969,
-             0.87348747]
-        ])
-
 
 class LothBaker2013ApplyCorrelationTestCase(unittest.TestCase):
     SITECOL = SiteCollection([Site(Point(2, -40), 1, 1, 1),
@@ -166,6 +146,172 @@ class LothBaker2013ApplyCorrelationTestCase(unittest.TestCase):
             decimal=6
         )
 
+
+class WangDu2013LowerTriangleCorrelationMatrixTestCase(unittest.TestCase):
+    SITECOL = SiteCollection([Site(Point(2, -40), 1, 1, 1),
+                              Site(Point(2, -40.1), 1, 1, 1),
+                              Site(Point(2, -39.9), 1, 1, 1)])
+
+    def test_sa_sa(self):
+        cormo = WangDu2013CorrelationModel(Rvs30=12.5)
+        imts = [SA(period=0.5, damping=5), SA(period=5.0, damping=5)]
+        lt = cormo.get_lower_triangle_correlation_matrix(self.SITECOL, imts)
+        aaae(lt, [
+            [1.0, 0, 0, 0, 0, 0],
+            [0.25654938, 0.96653113, 0, 0, 0, 0],
+            [0.25654938, 0.08330161, 0.96293471, 0, 0, 0],
+            [0.29,       0.02454221, 0.02251077, 0.95644704, 0, 0],
+            [0.09812013, 0.27399774, 0.01039717, 0.40868327, 0.8649668, 0],
+            [0.09812013, 0.0339733,  0.27208211, 0.40868327, 0.0797542,
+             0.86128209]
+        ])
+
+    def test_pga_pgv(self):
+        cormo = WangDu2013CorrelationModel(Rvs30=12.5)
+        imts = [PGA(), PGV()]
+        lt = cormo.get_lower_triangle_correlation_matrix(self.SITECOL, imts)
+        aaae(lt, [
+            [1.0, 0, 0, 0, 0, 0],
+            [0.22385942,  0.97462144, 0, 0, 0, 0],
+            [0.22385942,  0.06754524,  0.97227805, 0, 0, 0],
+            [0.91,       -0.01021082, -0.00952607, 0.41437301, 0, 0],
+            [0.19376039,  0.8891913,  -0.00410187,  0.03917511,  0.41260662, 0],
+            [0.19376039,  0.05753257,  0.88733759,  0.03917511,  0.01157218,
+             0.41244431]
+        ])
+
+class WangDu2013ApplyCorrelationTestCase(unittest.TestCase):
+    SITECOL = SiteCollection([Site(Point(2, -40), 1, 1, 1),
+                              Site(Point(2, -40.1), 1, 1, 1),
+                              Site(Point(2, -39.9), 1, 1, 1)])
+
+    def test_sa_sa(self):
+        numpy.random.seed(13)
+        cormo = WangDu2013CorrelationModel(Rvs30=12.5)
+
+        # Two IMTs --> cross-IMT correlation
+        imts = [SA(period=0.5, damping=5), SA(period=5.0, damping=5)]
+        num_sites = len(self.SITECOL)
+        num_imts = len(imts)
+        num_realizations = 100000
+
+        # Sample uncorrelated residuals
+        intra_residuals_sampled = numpy.random.normal(
+            size=(num_sites * num_imts, num_realizations))
+
+        # Apply correlation
+        intra_residuals_correlated = cormo.apply_correlation(
+            self.SITECOL, imts, intra_residuals_sampled
+        )
+
+        # Flatten IMT x site for correlation computation
+        flattened = intra_residuals_correlated.reshape(
+            num_sites * num_imts, num_realizations)
+        inferred_corrcoef = numpy.corrcoef(flattened)
+
+        # Check mean and std of correlated residuals
+        mean = intra_residuals_correlated.mean()
+        std = intra_residuals_correlated.std()
+        self.assertAlmostEqual(mean, 0, delta=0.002)
+        self.assertAlmostEqual(std, 1, delta=0.002)
+
+        # Compare to the correlation matrix from the model
+        actual_corrcoef = cormo._get_correlation_matrix(self.SITECOL, imts)
+        numpy.testing.assert_almost_equal(inferred_corrcoef, actual_corrcoef,
+                                          decimal=2)
+
+    def test_pga_pgv(self):
+        numpy.random.seed(13)
+        cormo = WangDu2013CorrelationModel(Rvs30=12.5)
+
+        # Two IMTs --> cross-IMT correlation
+        imts = [PGA(), PGV()]
+        num_sites = len(self.SITECOL)
+        num_imts = len(imts)
+        num_realizations = 100000
+
+        # Sample uncorrelated residuals
+        intra_residuals_sampled = numpy.random.normal(
+            size=(num_sites * num_imts, num_realizations))
+
+        # Apply correlation
+        intra_residuals_correlated = cormo.apply_correlation(
+            self.SITECOL, imts, intra_residuals_sampled
+        )
+
+        # Flatten IMT x site for correlation computation
+        flattened = intra_residuals_correlated.reshape(
+            num_sites * num_imts, num_realizations)
+        inferred_corrcoef = numpy.corrcoef(flattened)
+
+        # Check mean and std of correlated residuals
+        mean = intra_residuals_correlated.mean()
+        std = intra_residuals_correlated.std()
+        self.assertAlmostEqual(mean, 0, delta=0.002)
+        self.assertAlmostEqual(std, 1, delta=0.002)
+
+        # Compare to the correlation matrix from the model
+        actual_corrcoef = cormo._get_correlation_matrix(self.SITECOL, imts)
+        numpy.testing.assert_almost_equal(inferred_corrcoef, actual_corrcoef,
+                                          decimal=2)
+
+    def test_filtered_sitecol_sa_sa(self):
+        filtered = self.SITECOL.filtered([0, 2])
+        filtered.indices = numpy.array([0, 2])
+
+        numpy.random.seed(13)
+        cormo = WangDu2013CorrelationModel(Rvs30=12.5)
+        imts = [SA(period=0.5, damping=5), SA(period=5.0, damping=5)]
+
+        num_imts = len(imts)
+        num_sites_filtered = len(filtered)
+        num_realizations = 5
+
+        # Sample uncorrrelated residuals
+        intra_residuals_sampled = numpy.random.normal(
+            size=(num_sites_filtered * num_imts, num_realizations))
+        # Apply correlation
+        intra_residuals_correlated = cormo.apply_correlation(
+            filtered, imts, intra_residuals_sampled)
+
+        # Assert expected correlated residuals
+        aaae(intra_residuals_correlated, [
+            [[-0.71239066,  0.75376638, -0.04450308,  0.45181234,  1.34510171],
+             [ 0.32984325,  1.49352109,  0.81787309,  1.53979   , -0.66154492]],
+            [[-0.94923617, -0.95767325,  0.54481378, -0.06841674,  1.24049184],
+             [0.0258218 , 0.03536842, 2.31206596, 0.86939802, 0.19792573]],
+        ],
+            decimal=6
+        )
+
+    def test_filtered_sitecol_pga_pgv(self):
+        filtered = self.SITECOL.filtered([0, 2])
+        filtered.indices = numpy.array([0, 2])
+
+        numpy.random.seed(13)
+        cormo = WangDu2013CorrelationModel(Rvs30=12.5)
+        imts = [PGA(), PGV()]
+
+        num_imts = len(imts)
+        num_sites_filtered = len(filtered)
+        num_realizations = 5
+
+        # Sample uncorrrelated residuals
+        intra_residuals_sampled = numpy.random.normal(
+            size=(num_sites_filtered * num_imts, num_realizations))
+        # Apply correlation
+        intra_residuals_correlated = cormo.apply_correlation(
+            filtered, imts, intra_residuals_sampled)
+
+        # Assert expected correlated residuals
+        aaae(intra_residuals_correlated, [
+            [[-0.71239066,  0.75376638, -0.04450308,  0.45181234,  1.34510171],
+             [ 0.35810508,  1.48149576,  0.82737448,  1.53883613, -0.71528354]],
+            [[-0.98028235,  0.15028997,  0.18452676,  0.29623533,  1.61263038],
+            [ 0.43431117,  1.34720451,  1.6645251 ,  1.64016475, -0.64222091]],
+        ],
+            decimal=6
+        )
 
 class MarkhvidaEtAl2018LowerTriangleCorrelationMatrixTestCase(
         unittest.TestCase):
